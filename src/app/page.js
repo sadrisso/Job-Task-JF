3ac141a6f0df2f146f1375898213f6d223cedf6e
sadrisso <sadriisso304@gmail.com>
2025-07-28 12:50:26 +0600

    made a reuseable component and rendered two section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,100 @@
-import AgriculturalProducts from "@/components/AgriculturalProducts";
+
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import Products from "@/components/Products";
 
 export default function Home() {
+  const products = [
+    {
+      title: "FGC Beesthe Tonding 80s",
+      image: "https://i.ibb.co/xKcTq4V9/image4.jpg",
+      price: "₱4,950.00 - ₱18,850.00",
+      button: "Shop Now",
+    },
+    {
+      title: "GI Footly Mesh",
+      image: "https://i.ibb.co/v4N8ZLTK/farming-field-acre-topsoil.jpg",
+      price: "₱1,260.00 - ₱10,750.00",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Bluelink Mesh",
+      image: "https://i.ibb.co/YV3YMzq/jaipur-krishi-engineers.jpg",
+      price: "₱3,067.50 - ₱6,325.50",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Colbed Wire-21D 0681",
+      image: "https://i.ibb.co/8DGmQT0z/aspee-paddy-thresher-15-05-2023-096-272454904-j6s6r3x8.png",
+      price: "₱4,950.00",
+      button: "Shop Now",
+    },
+    {
+      title: "FGC Beesthe Tonding 80s",
+      image: "https://i.ibb.co/nqpQKxqY/H2d1508deb601404fa44f0a054fcf26524.jpg",
+      price: "₱4,950.00 - ₱18,850.00",
+      button: "Shop Now",
+    },
+    {
+      title: "GI Footly Mesh",
+      image: "https://i.ibb.co/wrNv0WFL/agriculture-equipment-196.jpg",
+      price: "₱1,260.00 - ₱10,750.00",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Colbed Wire-21D 0681",
+      image: "https://i.ibb.co/1YR7SDcB/hardware-bolts.png",
+      price: "₱4,950.00",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Bluelink Mesh",
+      image: "https://i.ibb.co/wF2Fhdpp/pvc-tubing-double-layer.png",
+      price: "₱3,067.50 - ₱6,325.50",
+      button: "Shop Now",
+    },
+  ];
+
+  const landscapeProducts = [
+    {
+      title: "FGC Beesthe Tonding 80s",
+      image: "https://i.ibb.co/vCrVNyFL/lands-1.jpg",
+      price: "₱4,950.00 - ₱18,850.00",
+      button: "Shop Now",
+    },
+    {
+      title: "GI Footly Mesh",
+      image: "https://i.ibb.co/jPNbTt8Q/lands-2.jpg",
+      price: "₱1,260.00 - ₱10,750.00",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Bluelink Mesh",
+      image: "https://i.ibb.co/Df6nbn8v/lands-3.webp",
+      price: "₱3,067.50 - ₱6,325.50",
+      button: "Shop Now",
+    },
+    {
+      title: "JF Colbed Wire-21D 0681",
+      image: "https://i.ibb.co/QF8YqYSf/lands-4.jpg",
+      price: "₱4,950.00",
+      button: "Shop Now",
+    },
+    {
+      title: "FGC Beesthe Tonding 80s",
+      image: "https://i.ibb.co/pjNtk4Hy/lands-5.jpg",
+      price: "₱4,950.00 - ₱18,850.00",
+      button: "Shop Now",
+    },
+    {
+      title: "GI Footly Mesh",
+      image: "https://i.ibb.co/hJB3PDnf/lands-6.jpg",
+      price: "₱1,260.00 - ₱10,750.00",
+      button: "Shop Now",
+    },
+  ];
+
+
   return (
     <div className="bg-white text-black">
       <div>
@@ -12,7 +104,11 @@ export default function Home() {
         <HeroSection />
       </div>
       <div>
-        <AgriculturalProducts />
+        <Products title="Agricultural Products" products={products} />
+      </div>
+
+      <div>
+        <Products title="Landscape Products" products={landscapeProducts} />
       </div>
     </div>
   );
---
+++ src/components/Products.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+
+const Products = ({ products, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  console.log("Props Products --> ", products);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % products.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + products.length) % products.length
+    );
+  };
+
+  return (
+    <div className="w-full py-12 px-4 bg-white">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-2xl md:text-4xl text-gray-800 mb-8 px-1">
+          {title}
+        </h2>
+        <div className="relative">
+          <div className="flex overflow-hidden">
+            <div
+              className="flex transition-transform duration-500 ease-in-out"
+              style={{ transform: `translateX(-${currentIndex * 25}%)` }}
+            >
+              {products.map((product, index) => (
+                <div key={index} className="w-1/4 flex-shrink-0 px-2">
+                  <div className="bg-[#F2F4F6] rounded-lg shadow-md p-4 border border-gray-200">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="w-full h-48 object-cover rounded-lg mb-4"
+                    />
+                    <h3 className="text-lg font-semibold text-gray-700">
+                      {product.title}
+                    </h3>
+                    <p className="text-gray-500 mb-4">{product.price}</p>
+                    <button className="bg-[#B0DD1D] px-2 md:px-4 py-1 md:py-2 rounded-lg hover:bg-green-400 text-xs">
+                      {product.button}
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button
+            onClick={prevSlide}
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-300 text-gray-700 p-2 rounded-full hover:bg-gray-400"
+          >
+            &lt;
+          </button>
+          <button
+            onClick={nextSlide}
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-300 text-gray-700 p-2 rounded-full hover:bg-gray-400"
+          >
+            &gt;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
